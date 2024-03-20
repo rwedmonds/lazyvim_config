29656ca34e995bcd6ddff7990491ff133de503cf
Robert Edmonds <redmonds@arista.com>
2024-03-20 07:50:42 -0500

    Update colorscheme and splash screen

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,7 +10,7 @@ return {
   --   },
   -- },
   -- add catppuccin mocha
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "catppuccin/nvim", name = "catppuccin-macchiato", priority = 1000 },
 
   {
     "LazyVim/LazyVim",
